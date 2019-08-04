83a0ac8dc7b8843bccf70edc2f7612319e669a04
alptureci90 <alp@accrualify.com>
2019-08-03 23:30:57 -0700

    started auto population

--- src/main/java/app/AutoPopulateDB.java
+++ src/main/java/app/AutoPopulateDB.java
@@ -20,7 +20,7 @@ public class AutoPopulateDB {
     public void fillAuthors(){
 
         Author author = new Author("Dan", "Brown");
-        pushRecord();
+        pushAuthor(author.getFirstName(), author.getLastName());
 
 
     }
@@ -37,15 +37,41 @@ public class AutoPopulateDB {
 
     }
 
-    public void pushRecord(){
-        String s = "INSERT INTO authors (firstName, lastName) values ('Dan','Brown')";
+    public void pushAuthor(String firstName, String lastName){
+        String s = "INSERT INTO authors (firstName, lastName) values (?,?)";
         try {
             PreparedStatement statement = connection.prepareStatement(s);
-            statement.execute();
+            statement.setString(1, firstName);
+            statement.setString(2, lastName);
+            executeStatement(statement);
         } catch (SQLException e){
             System.out.println("Error pushing record");
             System.out.println(e.getMessage());
         }
+    }
 
+    public void pushPublisher(String publisherName){
+        String s = "insert into publishers(publisherName) values (?)";
+        try {
+            PreparedStatement statement = connection.prepareStatement(s);
+            statement.setString(1, publisherName);
+        } catch (SQLException e){
+            System.out.println("Error in prepared statement");
+            e.getMessage();
+        }
+    }
+
+    public boolean executeStatement(PreparedStatement ps){
+        try{
+            ps.execute();
+            System.out.println("Execution Successfull;");
+            return true;
+        } catch (SQLException e){
+            System.out.println("Error in execution");
+            System.out.println(e.getMessage());
+            System.out.println("Closing Connection");
+            ConnectToDB.closeConnection(connection);
+            return false;
+        }
     }
 }
